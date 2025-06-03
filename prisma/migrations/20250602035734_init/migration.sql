3696720cced72fe4ea920ca87964d1ebc42d8d44
Bobby <alkaifkhawarizmi@gmail.com>
2025-06-03 20:32:15 +0530

    visitor functionality removed

--- prisma/migrations/20250602035734_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Visitor" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "ipAddress" TEXT,
-    "userAgent" TEXT,
-    "path" TEXT
-);
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,26 +1,18 @@
 "use client";
-
-import HomePage from "./homepage";
 import TopPanel from "./topPanel";
 import { useLockStore } from "../store/lockStore";
 import { usePowerStore } from "../store/powerStore";
 import LockScreen from "./lockScreen";
 import PowerScreen from "./powerScreen";
 import LeftSideBar from "./leftSideBar";
-import BrowserWindow from "./browserWindow";
 import RightSideBar from "./rightSideBar";
-import Visitors from "@/components/trackVisitor";
 
 export default function Main() {
   const { isLocked } = useLockStore();
   const { isPowerOn } = usePowerStore();
 
-
   return (
     <div>
-      <div className="absolute bottom-10 left-16 w-36 text-center h-10 rounded-2xl z-20">
-              <Visitors />
-            </div>
       {isPowerOn ? (
         <div>
           {isLocked ? (
